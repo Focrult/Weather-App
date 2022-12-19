49076704bb2e36ccb05e24befca823d29cfce086
Focrult <focrult@gmail.com>
2022-12-19 15:52:39 +0800

    Layed out JS and html, made comments on how to tackle this Challenge

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const test = "Hello WeatherApp!";
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+const API_KEY = 'a412b2680d9895a41ef3d8276e079247'
 
-console.log(test);
+//retrieve api
+// fetch weather data
+
+//hidden five cards?
+
+
+// display results
+
+// functionality:
+// button eventlistener for search results
+
+console.log("test");
+
+
+// localstorage to keep previous cities
+
+
