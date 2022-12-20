7618b5a1d450e1f9d94f4290e60b63af9b69d802
Focrult <focrult@gmail.com>
2022-12-20 15:25:43 +0800

    Playing around with the search, storing users cities

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
 const API_KEY = 'a412b2680d9895a41ef3d8276e079247'
-
+const submit = document.getElementById("submit");
+let userInput = document.getElementById("search-input");
+// const Temp = document.getElementById("");
+// const fiveDisplay = docuement.getElementById("");
+// const today = document.getElementById("");
+// var previous = document.getElementById("");
 //retrieve api
 // fetch weather data
 
@@ -12,9 +17,14 @@ const API_KEY = 'a412b2680d9895a41ef3d8276e079247'
 // functionality:
 // button eventlistener for search results
 
-console.log("test");
+$(".submit").click(function(){
+    localStorage.setItem("search", $(".search-input").val());
+    console.log("passing search");
+})
 
 
-// localstorage to keep previous cities
 
+//store city + API data
+$(".search-input").val(localStorage.getItem("search"));
+//localStorage.getItem("search-input");
 
