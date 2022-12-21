946f22f9d11d651f7e562ddfe091a61ad990998f
Focrult <focrult@gmail.com>
2022-12-21 14:11:46 +0800

    Added html and js drafts

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const API_KEY = 'a412b2680d9895a41ef3d8276e079247'
 const searchInput = document.getElementById('search-input');
+
 // const Temp = document.getElementById("");
 // const fiveDisplay = docuement.getElementById("");
 // const today = document.getElementById("");
@@ -23,13 +24,26 @@ function Display(){ //use code from previous challenge?
 // button eventlistener for search results
 
 $(".submit").click(function(){
+    const text = document.querySelector(".search-input").value;
     localStorage.setItem("search", $(".search-input").val());
     console.log("passing search");
-})
+    console.log(text);
+    $("#placeholder-name").text(text);
+});
 
+function Data(){
+    city: 
+}
+function Days("monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"){
+    if(Today == Days){
+        remove the day/.
+        Data();
+    }
+}
 
 
 //store city + API data
 $(".search-input").val(localStorage.getItem("search"));
+
 //localStorage.getItem("search-input");
 
