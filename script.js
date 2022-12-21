95e9088c9f3bf79589e44dea69fce776d534d92c
Focrult <focrult@gmail.com>
2022-12-21 15:33:35 +0800

    Fixed API URL to respond to user city inputgit add .!

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
 
 const API_KEY = 'a412b2680d9895a41ef3d8276e079247'
-// const searchInput = document.getElementById('search-input');
-searchInput = "perth";
-// const Temp = document.getElementById("");
-// const fiveDisplay = docuement.getElementById("");
-// const today = document.getElementById("");
-// var previous = document.getElementById("");
-//retrieve api
-// fetch weather data
+let searchInput = document.getElementsByClassName('.search-input');
+const newsearchInput = JSON.stringify(searchInput);
+console.log(newsearchInput)
 $(document).ready(function() {
+$(".submit").click(function(){
+    LiveWeather();
+    const text = document.querySelector(".search-input").value;
+    localStorage.setItem("search", $(".search-input").val());
+    console.log("passing search");
+    console.log(text);
+    $("#placeholder-name").text(text);
+    return text;
+    
+});
+$(".reset").click(function(){
+    localStorage.clear();
+});
+
+
+
+
+
+
+
+
+// fetch('requestUrl')
+// .then(function(response) {
+//     return response.json();
+// });
+// .then(function(data){
+//     console.log("testing \n------");
+//     for(let i=0; i<data.length; i++){
+//         console.log(data[i].city);
+//         console.log
+//     }
+// }
+
+
+$(".search-input").val(localStorage.getItem("search"));
+
 function LiveWeather(){
-    const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + API_KEY;
+    text = $(".search-input").val();
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=" + text + "&appid=" + API_KEY;
     console.log(API_URL);
+    return API_URL;
 }
 //hidden five cards?
-LiveWeather();
+
 
 // function Display(){ //use code from previous challenge? 
 
@@ -25,17 +58,7 @@ LiveWeather();
 // functionality:
 // button eventlistener for search results
 
-$(".submit").click(function(){
-    const text = document.querySelector(".search-input").value;
-    localStorage.setItem("search", $(".search-input").val());
-    console.log("passing search");
-    console.log(text);
-    $("#placeholder-name").text(text);
-    
-});
-$(".reset").click(function(){
-    localStorage.clear();
-});
+
 
 // const CityAdd = (e)=>{
 //     e.preventDefault();
@@ -66,10 +89,7 @@ $(".reset").click(function(){
 
 // resetSearch();
 //store city + API data
-$(".search-input").val(localStorage.getItem("search"));
+
 
 });
-function celConvert(){
-
-}
 
