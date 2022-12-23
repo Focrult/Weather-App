ebf188c46161ddea1cfedc5eda303f5cd0f7d852
Focrult <focrult@gmail.com>
2022-12-23 10:30:05 +0800

    Huge progress! Now tabs are created + five day forecastgit add . with iconsgit add .

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ $(".reset").click(function(){ //reset search input
 $(".search-input").val(localStorage.getItem("search"));
 function LiveWeather(){
     text = $(".search-input").val();
-    let API_URL = "https://api.openweathermap.org/data/2.5/weather?q=" + text + "&units=imperial&appid=" + API_KEY;
+    let API_URL = "https://api.openweathermap.org/data/2.5/weather?q=" + text + "&units=imperial&appid=" + API_KEY; //had &units=metric, however, I want this to match the gitlab img
     console.log(API_URL);
  
     var settings = { //for response
@@ -60,13 +60,11 @@ function LiveWeather(){
             let five = $(".forecast");
             for(i = 0; i < five.length; i++) {//should only fill the five divs!
             console.log(i);
-           
-                ///////CHANGE BELOW!!!!
                 // every three hours the weather array records data!
                 // we can ignore the first 4 elements use math equation?
                 // target 36, 28, 20, 12, 4 <- include these in the array for the five day forecast. Exclude 0-3
                 //         4, 3, 2, 1, 0
-                const index = [0, 1, 2, 3, 4];
+            const index = [0, 1, 2, 3, 4];
             const res1 = response.list[4];
             const res2 = response.list[12];
             const res3 = response.list[20];
@@ -74,7 +72,27 @@ function LiveWeather(){
             const res5 = response.list[36];
             console.log(res1);
             const array_S = ((i * (8)) + 4); //this should target the objects we want!
-            
+            let arrayDate = response.list[array_S].dt_txt.slice(0,10);
+            console.log(arrayDate);
+            const dates = document.createElement("h5");
+            dates.setAttribute("class", "mt-3 mb-2 forecast");
+            dates.innerHTML = arrayDate;
+            five[i].append(dates);
+            const icon = document.createElement("img"); //icon
+            icon.setAttribute("src", "https://openweathermap.org/img/wn/" + response.list[array_S].weather[0].icon + "@2x.png"); //review https://openweathermap.org/weather-conditions + http://openweathermap.org/img/wn/10d@2x.png
+            five[i].append(icon);
+            ////////////////////////////////
+            const forecastTempEl = document.createElement("h6");
+            forecastTempEl.innerHTML = "Temp: " + (response.list[array_S].main.temp) + " °F"; //use response from previous. add in Array_S formula to select only what we want!!!
+            five[i].append(forecastTempEl);
+            ////////////////////////////////
+            const forecastWind = document.createElement("h6");
+            forecastWind.innerHTML = "Wind: " + (response.list[array_S].wind.speed) + " MPH";
+            five[i].append(forecastWind);
+            ////////////////////////////////
+            const forecastHumidityEl = document.createElement("h6");
+            forecastHumidityEl.innerHTML = "Humidity: " + response.list[array_S].main.humidity + " %";
+            five[i].append(forecastHumidityEl);
             }
         })
 
@@ -82,106 +100,12 @@ function LiveWeather(){
 
     });
 
-        //five days forecast
-
-
-
    }//END OF FUNCTION LIVEWEATHER
-
-
-
-    //instead of city try id
-
 
 
 
 
 })//END OF DOCUMENT
-
-//another section for five day forecast?
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//obtain the data from URL;
-
-
-
-
-
-//single out specific data
-
-//store the data into local storage
-
-//display data onto cards
-
-
-
-
-// fetch('requestUrl')
-// .then(function(response) {
-//     return response.json();
-// });
-// .then(function(data){
-//     console.log("testing \n------");
-//     for(let i=0; i<data.length; i++){
-//         console.log(data[i].city);
-//         console.log
-//     }
-// }
-//hidden five cards?
-
-
-// function Display(){ //use code from previous challenge? 
-
-// }
-
-// display results
-
-// functionality:
-// button eventlistener for search results
-
-
-
-// const CityAdd = (e)=>{
-//     e.preventDefault();
-//     let city = {
-//         id:
-//         weather:
-//         humidity:
-//         wind: 
-
-
-//     }
-// }
-
-
-
-//add onto existing array
-
-
-// function Data(){
-//     city: 
-// }
-// function Days("monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"){
-//     if(Today == Days){
-//         remove the day/.
-//         Data();
-//     }
-// }
-
-// resetSearch();
-//store city + API data
 
 
 
